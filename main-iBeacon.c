22de4d606e299526bba0468419f3ee530e82c4db
Pariwat Leamsumran <leamsumran.p@gmail.com>
2015-12-29 11:45:15 +0700

    ตัวอย่างการปล่อยสัญญาณ iBeacon
    ตัวอย่างการปล่อยสัญญาณ iBeacon สำหรับ STM32-Shield

---
+++ main-iBeacon.c
@@ -0,0 +1,246 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  CL
+  * @version V1.0.0
+  * @date    04-July-2014
+  * @brief   This application contains an example which shows how implementing
+  *          a proprietary Bluetooth Low Energy profile: the sensor profile.
+  *          The communication is done using a Nucleo board and a Smartphone
+  *          with BTLE.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "cube_hal.h"
+
+#include "osal.h"
+#include "sensor_service.h"
+#include "debug.h"
+#include "stm32_bluenrg_ble.h"
+#include "bluenrg_utils.h"
+
+/** @addtogroup X-CUBE-BLE1_Applications
+ *  @{
+ */
+
+/** @defgroup SensorDemo
+ *  @{
+ */
+
+/** @defgroup MAIN 
+ * @{
+ */
+
+/** @defgroup MAIN_Private_Defines 
+ * @{
+ */
+/* Private defines -----------------------------------------------------------*/
+#define BDADDR_SIZE 6
+#define ADV_INTERVAL_MIN_MS    2000
+#define ADV_INTERVAL_MAX_MS    10000
+
+/**
+ * @}
+ */
+ 
+/* Private macros ------------------------------------------------------------*/
+
+/** @defgroup MAIN_Private_Variables
+ * @{
+ */
+/* Private variables ---------------------------------------------------------*/
+uint8_t bnrg_expansion_board = IDB04A1; /* at startup, suppose the X-NUCLEO-IDB04A1 is used */
+/**
+ * @}
+ */
+
+/** @defgroup MAIN_Private_Function_Prototypes
+ * @{
+ */
+/* Private function prototypes -----------------------------------------------*/
+
+/**
+ * @}
+ */
+
+/**
+ * @brief  Main function to show how to use the BlueNRG Bluetooth Low Energy
+ *         expansion board to send data from a Nucleo board to a smartphone
+ *         with the support BLE and the "BlueNRG" app freely available on both
+ *         GooglePlay and iTunes.
+ *         The URL to the iTunes for the "BlueNRG" app is
+ *         http://itunes.apple.com/app/bluenrg/id705873549?uo=5
+ *         The URL to the GooglePlay is
+ *         https://play.google.com/store/apps/details?id=com.st.bluenrg
+ *         The source code of the "BlueNRG" app, both for iOS and Android, is
+ *         freely downloadable from the developer website at
+ *         http://software.g-maps.it/
+ *         The board will act as Server-Peripheral.
+ *
+ *         After connection has been established:
+ *          - by pressing the USER button on the board, the cube showed by
+ *            the app on the smartphone will rotate.
+ *          
+ *         The communication is done using a vendor specific profile.
+ *
+ * @param  None
+ * @retval None
+ */
+int main(void)
+{
+  uint8_t SERVER_BDADDR[] = {0x12, 0x34, 0x00, 0xE1, 0x80, 0x03};
+  uint8_t bdaddr[BDADDR_SIZE];
+  uint16_t service_handle, dev_name_char_handle, appearance_char_handle;
+  
+  uint8_t  hwVersion;
+  uint16_t fwVersion;
+  
+  const uint8_t custom_ad[] = {26, AD_TYPE_MANUFACTURER_SPECIFIC_DATA,
+		0x4C, 0x00, //Company identifier code (Default is 0x004C - Apple, Inc.)
+		0x02, // ID
+		0x15, //Length of the remaining payload
+		0xE2, 0x0A, 0x39, 0xF4, 0x73, 0xF5, 0x4B, 0xC4, //Location UUID
+		0xA1, 0x2F, 0x17, 0xD1, 0xAD, 0x07, 0xA9, 0x61,
+		0x00, 0x00, // Major number
+		0x00, 0x00, // Minor number
+		0xC8 //2's complement of the Tx power (-56dB)};
+  };
+
+
+  int ret;  
+  
+  /* STM32Cube HAL library initialization:
+   *  - Configure the Flash prefetch, Flash preread and Buffer caches
+   *  - Systick timer is configured by default as source of time base, but user 
+   *    can eventually implement his proper time base source (a general purpose 
+   *    timer for example or other time source), keeping in mind that Time base 
+   *    duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+   *    handled in milliseconds basis.
+   *  - Low Level Initialization
+   */
+  HAL_Init();
+  
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize the BlueNRG SPI driver */
+  BNRG_SPI_Init();
+  
+  /* Initialize the BlueNRG HCI */
+  HCI_Init();
+
+  /* Reset BlueNRG hardware */
+  BlueNRG_RST();
+    
+  /* get the BlueNRG HW and FW versions */
+  getBlueNRGVersion(&hwVersion, &fwVersion);
+
+  /* 
+   * Reset BlueNRG again otherwise we won't
+   * be able to change its MAC address.
+   * aci_hal_write_config_data() must be the first
+   * command after reset otherwise it will fail.
+   */
+  BlueNRG_RST();
+  
+  PRINTF("HWver %d, FWver %d", hwVersion, fwVersion);
+  
+  if (hwVersion > 0x30) { /* X-NUCLEO-IDB05A1 expansion board is used */
+    bnrg_expansion_board = IDB05A1; 
+    /*
+     * Change the MAC address to avoid issues with Android cache:
+     * if different boards have the same MAC address, Android
+     * applications unless you restart Bluetooth on tablet/phone
+     */
+    SERVER_BDADDR[5] = 0x02;
+  }
+
+  /* The Nucleo board must be configured as SERVER */
+  Osal_MemCpy(bdaddr, SERVER_BDADDR, sizeof(SERVER_BDADDR));
+  
+  ret = aci_hal_write_config_data(CONFIG_DATA_PUBADDR_OFFSET,
+                                  CONFIG_DATA_PUBADDR_LEN,
+                                  bdaddr);
+  if(ret){
+    PRINTF("Setting BD_ADDR failed.\n");
+  }
+  
+  ret = aci_gatt_init();    
+  if(ret){
+    PRINTF("GATT_Init failed.\n");
+  }
+
+  if (bnrg_expansion_board == IDB05A1) {
+    ret = aci_gap_init_IDB05A1(GAP_PERIPHERAL_ROLE_IDB05A1, 0, 0x07, &service_handle, &dev_name_char_handle, &appearance_char_handle);
+  }
+  else {
+    ret = aci_gap_init_IDB04A1(GAP_PERIPHERAL_ROLE_IDB04A1, &service_handle, &dev_name_char_handle, &appearance_char_handle);
+  }
+
+  if(ret != BLE_STATUS_SUCCESS){
+    PRINTF("GAP_Init failed.\n");
+  }
+  
+
+  aci_gap_set_discoverable(ADV_NONCONN_IND,  (ADV_INTERVAL_MIN_MS*1000)/625, (ADV_INTERVAL_MAX_MS*1000)/625, PUBLIC_ADDR, NO_WHITE_LIST_USE, 0, NULL, 0, NULL, 0, 0);
+
+  ret = aci_gap_delete_ad_type(AD_TYPE_TX_POWER_LEVEL);
+  
+  /* Set the beacon custom data on the advertising packet */
+  ret = aci_gap_update_adv_data(sizeof(custom_ad), custom_ad);
+  
+  while(1)
+  {
+    HCI_Process();
+  }
+}
+
+/**
+ * @brief  Process user input (i.e. pressing the USER button on Nucleo board)
+ *         and send the updated acceleration data to the remote client.
+ *
+ * @param  AxesRaw_t* p_axes
+ * @retval None
+ */
+
+/**
+ * @}
+ */
+ 
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
